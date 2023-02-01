f5414652eb9b8dbc84132c4ae6592561536b87f0
MattEnricoFlores <mattenricoflores2022@gmail.com>
2023-02-01 10:29:17 +0200

    Initial commit

---
+++ layout/contactus.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Contact Us";
+include "header.php" ?>
+
+<h2>Contact Us</h2>
+
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ducimus in incidunt quaerat quam, ab pariatur rerum et modi. Nemo, inventore sint! Non ex similique fuga molestiae nisi aspernatur assumenda.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iusto ut, adipisci non autem totam illum id aliquam velit dolores! Vel nisi, nesciunt itaque laudantium accusamus repellendus cupiditate suscipit quia?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis repudiandae ab deserunt molestias, harum doloremque nam, cum, neque praesentium dignissimos iure voluptatibus odit fugit eius sint ut suscipit dolor.
+</p>
+<?php include "footer.php" ?>
---
+++ layout/index.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Home Page";
+include "header.php" ?>
+
+<h1>Some Content</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis atque ea asperiores doloremque, aut eaque! Voluptate perspiciatis optio, aliquid molestiae eum nulla impedit perferendis facere aliquam! Fuga, obcaecati repellendus!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis atque ea asperiores doloremque, aut eaque! Voluptate perspiciatis optio, aliquid molestiae eum nulla impedit perferendis facere aliquam! Fuga, obcaecati repellendus!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis atque ea asperiores doloremque, aut eaque! Voluptate perspiciatis optio, aliquid molestiae eum nulla impedit perferendis facere aliquam! Fuga, obcaecati repellendus!</p>
+
+
+
+<?php include "footer.php" ?>
