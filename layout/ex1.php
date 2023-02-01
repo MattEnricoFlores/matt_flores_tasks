1b3a68e0a6ecb69f613976be7ddd8ba33d8f64eb
MattEnricoFlores <mattenricoflores2022@gmail.com>
2023-02-01 12:18:09 +0200

    Fixed text in ex1

--- layout/ex1.php
+++ layout/ex1.php
@@ -7,14 +7,14 @@ include "header.php" ?>
 <h3>3.1 Write a simple PHP script to print your information (Name and your groupid).</h3>
 
 <?php
-echo '<span style="color:#FFFFFF"> Matt Flores <br></span>';
-echo '<span style="color:#FFFFFF">BBCAP22</span>';
+echo "Matt Flores";
+echo "BBCAP22";
 ?>
 
 <h3>3.2 Write PHP code to display the following message.</h3>
 
 <?php
-echo '<span style="color:#FFFFFF">Hello world! My name is "David"</span>';
+echo "Hello world! My name is \"David\"";
 ?>
 
 <h3>3.3 Write the PHP code in to display the current date.</h3>
